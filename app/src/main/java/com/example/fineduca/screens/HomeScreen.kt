14a96e8064f0222791722cc9db1bc2abb8474a6b
Fabio Estevão <fabio101515@gmail.com>
2025-03-15 22:06:27 -0300

    Merge remote-tracking branch 'origin/master' att: create home Screen

--- app/src/main/java/com/example/fineduca/screens/HomeScreen.kt
+++ app/src/main/java/com/example/fineduca/screens/HomeScreen.kt
@@ -1,2 +1,66 @@
 package com.example.fineduca.screens
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.fineduca.R
+import com.example.fineduca.components.BottomMenu
+import com.example.fineduca.components.TextBox
+import com.example.fineduca.components.TopMenu
 
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.main_blue))
+    ) {
+        // Menu superior
+        Spacer(modifier = Modifier.height(40.dp))
+        TopMenu()
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Conteúdo principal
+        Column(
+            modifier = Modifier
+                .weight(1f) // Ocupa o máximo de espaço disponível
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextBox(
+                title = "Título 1",
+                text = "Caixa 1 - Navegar para outra tela",
+                onClick = { println("Caixa 1 clicada!") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextBox(
+                title = "Título 2",
+                text = "Caixa 2 - Configurações",
+                onClick = { println("Caixa 2 clicada!") }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextBox(
+                title = "Título 3",
+                text = "Caixa 3 - Configurações",
+                onClick = { println("Caixa 3 clicada!") }
+            )
+        }
+
+        // Menu inferior
+        BottomMenu(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
