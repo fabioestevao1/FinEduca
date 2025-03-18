1bd7a11861a00e041cfbd26183d8283206bf5b51
BHzdev <bruno_tcc_tc@hotmail.com>
2025-03-17 23:34:10 -0300

    fix: compose-material version

--- app/src/main/java/com/example/fineduca/MainActivity.kt
+++ app/src/main/java/com/example/fineduca/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.fineduca
 
-//import LoginScreen
-//import RegisterScreen
-//import PasswordScreen
-//import com.example.fineduca.screens.QuotationScreen
-import com.example.fineduca.screens.HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.fineduca.ui.theme.FinEducaTheme
+import com.example.fineduca.ui.theme.Poppins
+import com.example.fineduca.screens.MainScreen
+import com.example.fineduca.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,29 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    HomeScreen() // Adicione a HomeScreen aqui
-
-//                    QuotationScreen()
-
-//                    RegisterScreen(
-//                        onBack = { /* Handle back action */ },
-//                        onNext = { name, email, password ->
-//                            // Handle login logic
-//                        }
-//                    )
-//                    LoginScreen(
-//                        onBack = { /* Handle back action */ },
-//                        onLogin = { email, password ->
-//                            // Handle login logic
-//                        }
-//                    )
-//                    PasswordScreen(
-//                        onBack = { /* Handle back action */ },
-//                        onNext = { email ->
-//                            // Handle login logic
-//                        }
-//                    )
+                    MainScreen();
                 }
             }
         }
